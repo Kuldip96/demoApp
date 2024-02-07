203f88fb89b81512c9038cfb11ca7172156933dc
Kuldip5078 <bhuvakuldip34@gmail.com>
2024-02-07 17:02:47 +0530

    fe

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:demo_app/firebase_options.dart';
 import 'package:demo_app/view/home/home_screen.dart';
 import 'package:demo_app/view/navigator_screen.dart';
 import 'package:demo_app/view/page_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
